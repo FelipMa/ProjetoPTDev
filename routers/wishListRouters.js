012285114a2710945683a90a24ab985524959079
Felipe Ma <felipeapenburg@gmail.com>
2022-08-15 14:59:40 -0300

    refactor links

--- routers/wishListRouters.js
+++ routers/wishListRouters.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router()
 const controller = require("../controllers/wishListControllers");
 
-router.get("/list-products/add-to-wish-list/:productId", function(req, res){
+router.get("/wish-list/add-to-wish-list/:productId", function(req, res){
     controller.addToWishLsit(req, res)
 })
-router.get("/list-products/remove-from-wish-list/:productId", function(req, res){
+router.get("/wish-list/remove-from-wish-list/:productId", function(req, res){
     controller.removeFromWishList(req, res)
 })
-router.get("/wishList", function (req, res){
+router.get("/wish-list", function (req, res){
     controller.getWishList(req, res)
 })
 
